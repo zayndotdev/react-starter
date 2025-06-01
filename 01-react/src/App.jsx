41591892128db449da50fe458c14454abfd888c9
Zayn Raza <zaynraza.dev@gmail.com>
2025-06-02 02:48:36 +0500

    React learning started

---
+++ 01-react/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-950 min-h-screen w-full flex justify-center items-center">
+      <h1 className="text-6xl text-white">console.log("It's working...")</h1>
+    </div>
+  );
+}
+
+export default App;
